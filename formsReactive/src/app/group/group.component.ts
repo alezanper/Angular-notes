7133a7ae6fe213d0528c89e5631c8cd10c4f4359
alezanper <fabenavideso@gmail.com>
2018-12-25 16:21:38 -0500

    Forms Reactive Validators

--- formsReactive/src/app/group/group.component.ts
+++ formsReactive/src/app/group/group.component.ts
@@ -8,19 +8,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class GroupComponent implements OnInit {
 
+  badNames = ['Alex', 'Maria']
+
   constructor() { }
 
   ngOnInit() {
   }
 
   groupForm = new FormGroup({
-    nameControl: new FormControl(null, [Validators.required]),
+    nameControl: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
     lastNameControl: new FormControl(null, [Validators.required]),
     emailControl: new FormControl(null, [Validators.required, Validators.email])
   });
 
+  /**
+   * When form is submitted the data will be show it to console
+   */
   onSubmit() {
     console.warn(this.groupForm.value);
+  }
+
+  /**
+   * Validator for avoid forbidden names
+   * @param control 
+   */
+  forbiddenNames(control: FormControl): { [s: string]: boolean }{
+    if(this.badNames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true}
+    }
+    return null;
   }
 
 }
