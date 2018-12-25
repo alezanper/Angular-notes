dde25bdf41259c72577035a238b6afd98034ac19
alezanper <fabenavideso@gmail.com>
2018-12-25 17:04:33 -0500

    Forms Reactive Validators

--- formsReactive/src/app/group/group.component.ts
+++ formsReactive/src/app/group/group.component.ts
@@ -13,13 +13,27 @@ export class GroupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.onInitForm()
   }
 
+  // Defining group Form
   groupForm = new FormGroup({
+    // Undividual form controls including validators
     nameControl: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
     lastNameControl: new FormControl(null, [Validators.required]),
     emailControl: new FormControl(null, [Validators.required, Validators.email])
   });
+
+  /**
+   * For filling form with default values
+   */
+  onInitForm(){
+    this.groupForm.setValue({
+      'nameControl': 'some name',
+      'lastNameControl': 'some lastname',
+      'emailControl': 'email@email.com'
+    })
+  }
 
   /**
    * When form is submitted the data will be show it to console
